c47483f0973584ebb0f6197e05469f0def5a499d
cookie <cookiedistorted@gmail.com>
2024-10-24 18:53:13 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const audioContext = useRef<AudioContext | null>(null);
+  const audioGain = useRef<GainNode | null>(null);
+
+  const setupAudio = () => {
+    audioContext.current = new AudioContext();
+    const oscillator = audioContext.current?.createOscillator();
+    oscillator.type = 'sine';
+    audioGain.current = audioContext.current?.createGain();
+    oscillator.connect(audioGain.current);
+    audioGain.current?.connect(audioContext.current?.destination);
+    oscillator.start(0);
+    stop();
+  };
+
+  const start = () => {
+    console.log('start');
+    // if (!audioContext.current) setupAudio();
+    if (!audioContext.current) return;
+    if (!audioGain.current) return;
+
+    audioGain.current?.gain?.exponentialRampToValueAtTime(
+      0.95, audioContext.current?.currentTime + 0.05
+    );
+  };
+
+  const stop = () => {
+    // if (!audioContext.current) setupAudio();
+    if (!audioContext.current) return;
+    if (!audioGain.current) return;
+
+    audioGain.current?.gain?.exponentialRampToValueAtTime(
+      0.000001, audioContext.current?.currentTime + 0.05
+    );
+  };
+
+  useEffect(() => {
+    return () => {
+      stop();
+      // oscillator.stop(0);
+      // oscillator.disconnect();
+      audioContext.current?.close();
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onKeyDown={start} onKeyUp={stop}>
+      <div tabIndex={0} autoFocus onClick={setupAudio}>Start</div> 
     </div>
   );
 }
